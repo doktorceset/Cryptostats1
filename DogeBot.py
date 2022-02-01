28e453ba5f4f2112953d40cb2a40ed42f946a209
DoktorCeset <66803624+doktorceset@users.noreply.github.com>
2022-02-01 20:54:06 +0300

    Update DogeBot.py

--- DogeBot.py
+++ DogeBot.py
@@ -12,9 +12,9 @@ import random
 
 #Retrieving API tokens
 PORT = int(os.environ.get('PORT', 5000))
-TOKEN = os.environ['TOKEN']
-NEWS_TOKEN = os.environ['NEWS_TOKEN']
-CMC_TOKEN = os.environ['CMC_TOKEN']
+TOKEN = os.environ['2032391705:AAEn8QLbdcCPpo7dhTWPDzudB6FuTlhztDw']
+NEWS_TOKEN = os.environ['03c07fd3d77daedfe0a18b46842876f8ca10eb3c']
+CMC_TOKEN = os.environ['b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c']
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
